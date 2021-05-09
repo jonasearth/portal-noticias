b3a6bac75b6270061c3ef6a3c5ec6e0f7af79bac
David Bastos <davidtavares98@gmail.com>
2021-05-09 02:21:20 -0300

    Falta ajustart view

--- src/main/java/com/devleague/portalnoticias/View/Visualizador.java
+++ src/main/java/com/devleague/portalnoticias/View/Visualizador.java
@@ -8,19 +8,17 @@ import com.devleague.portalnoticias.DB.DB;
 import com.devleague.portalnoticias.Model.Noticia;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class Visualizador {
 
     public static DB db = new DB();
 
-    public static void espaco(){
+    public static void espaco() {
         System.out.println("===================================================");
     }
 
-    public static void exibir(){
+    public static void exibir() {
 
         while (true) {
             Scanner input = new Scanner(System.in);
@@ -32,7 +30,6 @@ public class Visualizador {
             espaco();
 
             int escolha = input.nextInt();
-            // input.close();
 
             switch (escolha) {
                 case 1:
@@ -54,76 +51,117 @@ public class Visualizador {
         }
     }
 
-    public static void criarPublicacao(){
+    public static void criarPublicacao() {
         Scanner input = new Scanner(System.in);
-//        Map<String, String> publicacao = new HashMap<String, String>();
 
         System.out.println("Autor: ");
         String autor = input.nextLine();
-//        publicacao.put("autor", autor);
 
         System.out.println("Titulo: ");
         String titulo = input.nextLine();
-//        publicacao.put("titulo", titulo);
 
         System.out.println("Categoria: ");
         String categoria = input.nextLine();
-//        publicacao.put("categoria", categoria);
 
         System.out.println("Conteudo: ");
         String conteudo = input.nextLine();
-//        publicacao.put("conteudo", conteudo);
-        CreateNoticiaController.add(db,titulo, conteudo, categoria, autor);
+
+        CreateNoticiaController.add(db, titulo, conteudo, categoria, autor);
 
     }
-    public static void buscarPublicacoes(){
-        ArrayList<Noticia> noticias =  GetNoticiaController.getAll(db);
-        for(int i = 0; i < noticias.size() ; i++)
-            System.out.println(i + " - "+noticias.get(i).getTitulo());
+
+    public static void buscarPublicacoes() {
+        ArrayList<Noticia> noticias = GetNoticiaController.getAll(db);
+        for (int i = 0; i < noticias.size(); i++)
+            System.out.println(i + " - " + noticias.get(i).getTitulo());
 
 
     }
-    public static void visualizarPublicacao(int index){
-        Noticia noticia =  GetNoticiaController.get(db, index);
-        if (noticia == null){
+
+    public static void visualizarPublicacao(int index) {
+        Noticia noticia = GetNoticiaController.get(db, index);
+        if (noticia == null) {
             System.out.println("Noticia não encontrada");
             return;
-        }else{
-            //visualização dos dados
         }
+        System.out.println(noticia.getTitulo());
+        System.out.println(noticia.getAuthor());
+        System.out.println(noticia.getCategoria());
+        System.out.println(noticia.getConteudo());
     }
-    public static void editarPublicacao(int index){
-        Noticia noticia =  GetNoticiaController.get(db, index);
-        if (noticia == null){
+
+    public static void editarPublicacao(int index) {
+        Noticia noticia = GetNoticiaController.get(db, index);
+        if (noticia == null) {
             System.out.println("Noticia não encontrada");
             return;
-        }else{
-            UpdateNoticiaController.update(db,index, noticia);
         }
+        espaco();
+        System.out.println("Editar publicação!");
+        System.out.println("1- Alterar Autor");
+        System.out.println("2- Alterar Título");
+        System.out.println("3- Alterar Publicação");
+        System.out.println("4- Alterar Conteudo");
+        System.out.println("0- Finalizar edição");
+        espaco();
+        boolean loop = true;
+        while (loop) {
+            Scanner input = new Scanner(System.in);
+            int escolha = input.nextInt();
+
+            switch (escolha) {
+                case 1:
+                    String autor = input.nextLine();
+                    noticia.setAuthor(autor);
+                    break;
+                case 2:
+                    String titulo = input.nextLine();
+                    noticia.setTitulo(titulo);
+                    break;
+                case 3:
+                    String categoria = input.nextLine();
+                    noticia.setCategoria(categoria);
+                    break;
+                case 4:
+                    String conteudo = input.nextLine();
+                    noticia.setConteudo(conteudo);
+                    break;
+                default:
+                    loop = false;
+                    break;
+            }
+        }
+
+        UpdateNoticiaController.update(db, index, noticia);
+
     }
-    public static void deletarPublicacao(int index){
-        boolean deleted =  DeleteNoticiaController.delete(db, index);
-        if (deleted){
+
+    public static void deletarPublicacao(int index) {
+        boolean deleted = DeleteNoticiaController.delete(db, index);
+        if (deleted) {
             System.out.println("Noticia Deletada com sucesso");
             return;
-        }else{
+        } else {
             System.out.println("Noticia Não pôde ser deletada");
             return;
         }
     }
-    public static void opcoesPublicacao(int index){
+
+    public static void opcoesPublicacao(int index) {
         Scanner input = new Scanner(System.in);
         espaco();
-        System.out.println("O que fazer com a publicação!");
+        System.out.println("Publicação");
         System.out.println("1- Visualizar Publicacao");
         System.out.println("2- Editar Publicacao");
         System.out.println("3- Deletar Publicacao");
-        System.out.println("0- se quiser sair do programa");
+        System.out.println("0- Sair de publicações");
         espaco();
-
+        
         int escolha = input.nextInt();
         switch (escolha) {
             case 1:
+                System.out.println("Index da noticia");
+//                int index = input.nextInt();
                 visualizarPublicacao(escolha);
                 break;
             case 2:
