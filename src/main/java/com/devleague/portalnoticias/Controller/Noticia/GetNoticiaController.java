d9be14310009598a5f167ca0245856bcad520838
jonas lima <jonasearth1@gmail.com>
2021-05-09 00:27:32 -0300

    finalizado noticia controller

--- src/main/java/com/devleague/portalnoticias/Controller/Noticia/GetNoticiaController.java
+++ src/main/java/com/devleague/portalnoticias/Controller/Noticia/GetNoticiaController.java
@@ -3,12 +3,22 @@ package com.devleague.portalnoticias.Controller.Noticia;
 import com.devleague.portalnoticias.DB.DB;
 import com.devleague.portalnoticias.Model.Noticia;
 
+import java.util.ArrayList;
+
 public class GetNoticiaController {
 
-    public static void get(DB db, int index) {
-
+    public static Noticia get(DB db, int index) {
+        try {
+            return Noticia.get(db, index);
+        }catch (Exception e){
+            return null;
+        }
     }
-    public static void getAll(DB db) {
-
+    public static ArrayList<Noticia> getAll(DB db) {
+        try {
+            return Noticia.getAll(db);
+        }catch (Exception e){
+            return null;
+        }
     }
 }
