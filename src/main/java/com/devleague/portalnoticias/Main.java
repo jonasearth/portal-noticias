dca561674d390f2e52b2570747dfa3d31eae10f9
VncsSz <vini_souzza@hotmail.com>
2021-05-19 00:31:09 -0300

    Criar Controllers para Categoria

--- src/main/java/com/devleague/portalnoticias/Main.java
+++ src/main/java/com/devleague/portalnoticias/Main.java
@@ -11,5 +11,6 @@ public class Main {
     public static void main(String[] args) {
         GetNoticiaController.get(db, 0);
         Visualizador.exibir();
+        System.out.println("Qualquer mensagem");
     }
 }
