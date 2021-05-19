ca679b551b162e990def835f459a3d7709971dd0
PeJoMa <20191bsifsa0022.pedro.filho@gmail.com>
2021-05-18 21:29:29 -0300

    Criar Model Categoria

---
+++ src/main/java/com/devleague/portalnoticias/Model/Categoria.java
@@ -0,0 +1,18 @@
+package com.devleague.portalnoticias.Model;
+
+public class Categoria {
+    private String nome;
+
+    public Categoria(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
