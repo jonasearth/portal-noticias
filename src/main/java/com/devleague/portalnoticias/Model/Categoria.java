821ef67ffb385e9db948a09f7dbf6da4b0b3c8c8
PeJoMa <20191bsifsa0022.pedro.filho@gmail.com>
2021-05-18 21:51:36 -0300

    Criar metodos de consulta ao db no Model Categoria

--- src/main/java/com/devleague/portalnoticias/Model/Categoria.java
+++ src/main/java/com/devleague/portalnoticias/Model/Categoria.java
@@ -1,5 +1,9 @@
 package com.devleague.portalnoticias.Model;
 
+import com.devleague.portalnoticias.DB.DB;
+
+import java.util.ArrayList;
+
 public class Categoria {
     private String nome;
 
@@ -15,4 +19,20 @@ public class Categoria {
         this.nome = nome;
     }
 
+
+    public static Categoria get(DB db, int index){
+        return db.categorias.get(index);
+    }
+    public static ArrayList<Categoria> getAll(DB db){
+        return db.categorias;
+    }
+    public static boolean create(DB db, Categoria categoria){
+        return  db.categorias.add(categoria);
+    }
+    public static Categoria update(DB db, int index, Categoria categoria){
+        return  db.categorias.set(index, categoria);
+    }
+    public static Categoria delete(DB db, int index){
+        return db.categorias.remove(index);
+    }
 }
