76d7192dde3bb6a9b3ce37347005f1372697b1e4
jonas lima <jonasearth1@gmail.com>
2021-06-11 14:56:36 -0300

    aaaaa

--- src/main/java/com/devleague/portalnoticias/Controller/Jornalista/CreateJornalistaController.java
+++ src/main/java/com/devleague/portalnoticias/Controller/Jornalista/CreateJornalistaController.java
@@ -4,11 +4,15 @@ import com.devleague.portalnoticias.DB.DB;
 import com.devleague.portalnoticias.Model.Jornalista;
 
 public class CreateJornalistaController {
+    //recebe 2 parametros do view , o db e jornalista
     public static boolean add(DB db, Jornalista jornalista) {
+        //previne quebra da aplicação por possiveis erros que possam existir no model
         try {
+            //tenta adicionar o jornalista ao banco de dados
             jornalista.create(db);
             return true;
         }catch (Exception e){
+            //retorna false caso algum erro aconteça
             return false;
         }
     }
